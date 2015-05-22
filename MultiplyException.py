1a0c717b78dc733c335039e7121aefcb10637232
cat-lucy <cat-lucy@mail.ru>
2015-05-22 08:21:36 +0300

    add exception

---
+++ MultiplyException.py
@@ -0,0 +1,20 @@
+def check_number(number):
+    if len(number) == 0:
+        err = MultiplyException('Empty input')
+        raise err
+    for i in range(len(number)):
+        if number[i] not in '0123456789':
+            err = MultiplyException('Invalid Input :')
+            err.arg = number
+            err.type = type(number)
+            raise err
+        return int(number)
+
+
+class MultiplyException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type = object
+        self.arg = ""
+
+    pass
